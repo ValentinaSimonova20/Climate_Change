9819bc2276958e06df783476283cb96d3ebdaca7
19924245 <vvlsimonova@sberbank.ru>
2023-04-11 12:18:41 +0300

    show articles images on ui

--- NewsApp/src/main/java/com/climatechange/newsapp/service/news/NewsServiceImpl.java
+++ NewsApp/src/main/java/com/climatechange/newsapp/service/news/NewsServiceImpl.java
@@ -1,10 +1,16 @@
 package com.climatechange.newsapp.service.news;
 
 import com.climatechange.newsapp.integration.api.NewsApi;
+import com.climatechange.newsapp.model.Article;
 import com.climatechange.newsapp.model.NewsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +22,23 @@ public class NewsServiceImpl implements NewsService {
 
     @Override
     public NewsData getNewsDataByQ(String q) {
-        return newsApi.getNewsData(q, newsApiKey);
+        NewsData newsData = newsApi.getNewsData(q, newsApiKey);
+        newsData.setArticles(newsData.getArticles().subList(0, 25)); // todo replace with pagination
+        validateImageUrls(newsData.getArticles());
+        return newsData;
     }
 
+    /**
+     * if the imageUrl not accessible then clear it
+     * @param articleList articles in which we check image urls
+     */
+    private void validateImageUrls(List<Article> articleList) {
+        articleList.forEach(article -> {
+            try {
+                ImageIO.read(new URL(article.getUrlToImage()));
+            } catch (IOException e) {
+                article.setUrlToImage("");
+            }
+        });
+    }
 }
